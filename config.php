02c176dfac662deb53a7533ddae2bb3a72b1fdd2
whanrott <eart0208@nexus.ox.ac.uk>
2016-03-18 10:08:31 +0000

    removed less folder

--- config.php
+++ config.php
@@ -38,7 +38,8 @@ $THEME->name = 'msdlt';
 //
 $THEME->doctype = 'html5';
 $THEME->parents = array('bootstrapbase');
-$THEME->sheets = array('custom');
+$THEME->sheets = array('base');
+$THEME->lessfile = 'moodle';
 $THEME->supportscssoptimisation = false;
 $THEME->yuicssmodules = array();
 $THEME->enable_dock = true;
